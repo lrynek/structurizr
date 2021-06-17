89015f5deb8ac00f92511573d6f1a4a6d1785cd2
Łukasz Rynek <lukasz.rynek@docplanner.com>
2021-06-17 12:07:15 +0200

    Full Search C1 diagram (stable)

--- send.php
+++ send.php
@@ -10,35 +10,67 @@ use StructurizrPHP\Client\Infrastructure\Http\SymfonyRequestFactory;
 use StructurizrPHP\Client\UrlMap;
 use StructurizrPHP\Core\Model\Enterprise;
 use StructurizrPHP\Core\Model\Location;
+use StructurizrPHP\Core\Model\Relationship\InteractionStyle;
 use StructurizrPHP\Core\Model\Tags;
 use StructurizrPHP\Core\View\Configuration\Shape;
 use StructurizrPHP\Core\Workspace;
 
 $workspace = new Workspace(
-	$id = (string)\getenv('STRUCTURIZR_WORKSPACE_ID'),
-	$name = 'DocPlanner SEARCH',
-	$description = 'Model of search system'
+	(string)\getenv('STRUCTURIZR_WORKSPACE_ID'),
+	 'DocPlanner SEARCH',
+	'Model of search system'
 );
-$workspace->getModel()->setEnterprise(new Enterprise('DocPlanner SEARCH'));
-$person = $workspace->getModel()->addPerson(
-	$name = 'Patient',
+$model = $workspace->getModel();
+$model->setEnterprise(new Enterprise('DocPlanner SEARCH'));
+
+# starts C1 system context diagram
+$person = $model->addPerson(
+	'Patient',
 	'',
 	Location::internal()
 );
-$softwareSystem = $workspace->getModel()->addSoftwareSystem(
-	$name = 'Search',
-	$description = 'Doctors and facilities ranked searching',
+$search = $model->addSoftwareSystem(
+	'Search',
+	'Doctors and facilities ranked searching',
 	Location::internal()
 );
-$person->usesSoftwareSystem($softwareSystem, 'Searches a medical service', 'HTTP');
+$person->usesSoftwareSystem($search, 'Searches a medical service', 'HTTP');
 
-$contextView = $workspace->getViews()->createSystemContextView($softwareSystem, 'System Context', '[C1] A System Context diagram');
+$googleAnalytics = $model->addSoftwareSystem(
+	'Google Analytics',
+	'',
+	Location::external()
+);
+$search->usesSoftwareSystem($googleAnalytics, 'Sends analytics data', '');
+$googleAnalytics->setTags(new Tags('External Software System'));
+
+$algolia = $model->addSoftwareSystem(
+	'Algolia',
+	'',
+	Location::external()
+);
+$search->usesSoftwareSystem($algolia, 'Asks for phrase autocomplete', '');
+$algolia->usesSoftwareSystem($search, 'Returns a list of matching autocomplete items', '');
+$algolia->setTags(new Tags('External Software System'));
+
+$googleMaps = $model->addSoftwareSystem(
+	'Google Maps',
+	'',
+	Location::external()
+);
+$search->usesSoftwareSystem($googleMaps, 'Asks for location autocomplete or custom coordinates', '');
+$googleMaps->usesSoftwareSystem($search, 'Returns a list of matching locations, renders map', '');
+$googleMaps->setTags(new Tags('External Software System'));
+
+$views = $workspace->getViews();
+$contextView = $views->createSystemContextView($search, 'System Context', '[C1] A System Context diagram');
 $contextView->addAllElements();
 $contextView->setAutomaticLayout(true);
+# ends C1 system context diagram
 
-$styles = $workspace->getViews()->getConfiguration()->getStyles();
-
+$styles = $views->getConfiguration()->getStyles();
 $styles->addElementStyle(Tags::SOFTWARE_SYSTEM)->background("#1168bd")->color('#ffffff');
+$styles->addElementStyle('External Software System')->background("#837d8c")->color('#ffffff');
 $styles->addElementStyle(Tags::PERSON)->background("#08427b")->color('#ffffff')->shape(Shape::person());
 
 $client = new Client(
